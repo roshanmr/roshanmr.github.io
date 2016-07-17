669035dd459a55c862e181d9da723ec873a87c3c
Roshan Ravi <roshanravi@Roshans-MacBook-Pro-2.local>
2016-07-18 00:04:48 +0530

    Updates to Process Inner Pages
    Made the sticky navigation standard on all inner pages of process

--- srijan/process-weekly-burn-rate.php
+++ srijan/process-weekly-burn-rate.php
@@ -324,13 +324,13 @@
                   </span>
                   <span class="title"><strong>1.</strong> Pre-Engagement</span>
                 </a>
-               <ul class="child-menu">
+                <ul class="child-menu">
                   <li><a href="process.php">
                     <i class="fa fa-arrow-right"></i>
                     <span>Empowered Teams</span>
                     </a>
                   </li>
-                  <li class="current"><a href="process-weekly-burn-rate.php">
+                  <li  class="current"><a href="process-weekly-burn-rate.php">
                     <i class="fa fa-arrow-right"></i>
                     <span>Weekly Burn Rate Model</span>
                     </a>
@@ -357,8 +357,32 @@
                   <span class="icon-small">
                     <?php echo file_get_contents("icons/design.svg"); ?>
                   </span>
-                  <span class="title"><strong>2.</strong> UX Discovery</span>
+                  <span class="title"><strong>2.</strong> UX &amp; UI Design</span>
                 </a>
+
+                <ul class="child-menu">
+                  <li><a href="process.php">
+                    <i class="fa fa-arrow-right"></i>
+                    <span>UX Discovery</span>
+                    </a>
+                  </li>
+                  <li><a href="process-weekly-burn-rate.php">
+                    <i class="fa fa-arrow-right"></i>
+                    <span>User Research</span>
+                    </a>
+                  </li>
+                  <li><a href="process-scrum-team.php">
+                    <i class="fa fa-arrow-right"></i>
+                    <span>UX Design &amp; Wireframing</span>
+                    </a>
+                  </li>
+                  <li><a href="process-agile-waterfall.php">
+                    <i class="fa fa-arrow-right"></i>
+                    <span>UI Design</span>
+                    </a>
+                  </li>
+                </ul>
+
               </li>
               <li>
                 <a href="#">
@@ -367,6 +391,19 @@
                   </span>
                   <span class="title"><strong>3.</strong> Prototyping</span>
                 </a>
+                 <ul class="child-menu">
+                  <li><a href="process.php">
+                    <i class="fa fa-arrow-right"></i>
+                    <span>Click-Through Prototypes</span>
+                    </a>
+                  </li>
+
+                  <li><a href="process.php">
+                    <i class="fa fa-arrow-right"></i>
+                    <span>HTML, CSS &amp; JS Prototypes</span>
+                    </a>
+                  </li>
+                </ul>
               </li>
               <li>
                 <a href="#">
@@ -375,6 +412,26 @@
                   </span>
                   <span class="title"><strong>4.</strong> Tech Discovery</span>
                 </a>
+                <ul class="child-menu">
+                  <li><a href="process.php">
+                    <i class="fa fa-arrow-right"></i>
+                    <span>Project Estimation</span>
+                    </a>
+                  </li>
+
+                  <li><a href="process.php">
+                    <i class="fa fa-arrow-right"></i>
+                    <span>Audit - Paid &amp; Free</span>
+                    </a>
+                  </li>
+
+                  <li><a href="process.php">
+                    <i class="fa fa-arrow-right"></i>
+                    <span>POC - Spike</span>
+                    </a>
+                  </li>
+                </ul>
+
               </li>
               <li>
                 <a href="#">
@@ -383,6 +440,34 @@
                   </span>
                   <span class="title"><strong>5.</strong> Development Sprints</span>
                 </a>
+
+                <ul class="child-menu">
+                  <li><a href="process.php">
+                    <i class="fa fa-arrow-right"></i>
+                    <span>Tracking Development on JIRA</span>
+                    </a>
+                  </li>
+
+                  <li><a href="process.php">
+                    <i class="fa fa-arrow-right"></i>
+                    <span>Backlog &amp; Sprint Planning</span>
+                    </a>
+                  </li>
+
+                  <li><a href="process.php">
+                    <i class="fa fa-arrow-right"></i>
+                    <span>Demos</span>
+                    </a>
+                  </li>
+
+                  <li><a href="process.php">
+                    <i class="fa fa-arrow-right"></i>
+                    <span>Retrospectives</span>
+                    </a>
+                  </li>
+                </ul>
+
+
               </li>
               <li>
                 <a href="#">
@@ -391,6 +476,26 @@
                   </span>
                   <span class="title"><strong>6.</strong> DevOps</span>
                 </a>
+
+                <ul class="child-menu">
+                  <li><a href="process.php">
+                    <i class="fa fa-arrow-right"></i>
+                    <span>Infrastructure</span>
+                    </a>
+                  </li>
+                  <li><a href="process.php">
+                    <i class="fa fa-arrow-right"></i>
+                    <span>Platform.sh</span>
+                    </a>
+                  </li>
+
+                  <li><a href="process.php">
+                    <i class="fa fa-arrow-right"></i>
+                    <span>Aquia Cloud</span>
+                    </a>
+                  </li>
+
+                </ul>
               </li>
               <li>
                 <a href="#">
@@ -399,6 +504,56 @@
                   </span>
                   <span class="title"><strong>7.</strong> QA and Testing</span>
                 </a>
+                <ul class="child-menu">
+                  <li><a href="process.php">
+                    <i class="fa fa-arrow-right"></i>
+                    <span>Realtime User Testing with Demo Script</span>
+                    </a>
+                  </li>
+                  <li><a href="process.php">
+                    <i class="fa fa-arrow-right"></i>
+                    <span>JMeter, New Relic, Blazemeter-Improving performance of a website</span>
+                    </a>
+                  </li>
+
+                  <li><a href="process.php">
+                    <i class="fa fa-arrow-right"></i>
+                    <span>Manual Testing</span>
+                    </a>
+                  </li>
+
+                  <li><a href="process.php">
+                    <i class="fa fa-arrow-right"></i>
+                    <span>Automated Testing</span>
+                    </a>
+                  </li>
+
+                  <li><a href="process.php">
+                    <i class="fa fa-arrow-right"></i>
+                    <span>Definition of Done</span>
+                    </a>
+                  </li>
+
+                  <li><a href="process.php">
+                    <i class="fa fa-arrow-right"></i>
+                    <span>Acceptance Criteria </span>
+                    </a>
+                  </li>
+
+                  <li><a href="process.php">
+                    <i class="fa fa-arrow-right"></i>
+                    <span>Performance Testing - Backfire </span>
+                    </a>
+                  </li>
+
+                  <li><a href="process.php">
+                    <i class="fa fa-arrow-right"></i>
+                    <span>Penetration Testing - Qwalus </span>
+                    </a>
+                  </li>
+
+                </ul>
+
               </li>
               <li>
                 <a href="#">
@@ -407,6 +562,23 @@
                   </span>
                   <span class="title"><strong>8.</strong> Communication</span>
                 </a>
+                 <ul class="child-menu">
+                  <li><a href="process.php">
+                    <i class="fa fa-arrow-right"></i>
+                    <span>Single View Status Report</span>
+                    </a>
+                  </li>
+                  <li><a href="process.php">
+                    <i class="fa fa-arrow-right"></i>
+                    <span>Retros and tools</span>
+                    </a>
+                  </li>
+                  <li><a href="process.php">
+                    <i class="fa fa-arrow-right"></i>
+                    <span>Burndown Charts for Agile Reporting</span>
+                    </a>
+                  </li>
+                </ul>
               </li>
               <li>
                 <a href="#">
@@ -415,8 +587,29 @@
                   </span>
                   <span class="title"><strong>9.</strong> Support</span>
                 </a>
+                <ul class="child-menu">
+                  <li><a href="process.php">
+                    <i class="fa fa-arrow-right"></i>
+                    <span>Model</span>
+                    </a>
+                  </li>
+
+                  <li><a href="process.php">
+                    <i class="fa fa-arrow-right"></i>
+                    <span>Sprint Planning</span>
+                    </a>
+                  </li>
+
+                  <li><a href="process.php">
+                    <i class="fa fa-arrow-right"></i>
+                    <span>Reporting</span>
+                    </a>
+                  </li>
+
+                </ul>
               </li>
             </ul>
+
           </aside>
           <article class="ps-main col-9">
            

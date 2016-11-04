0f1d0da81b58b91e5169844e9c433e6a56395983
Bharat <bharat.bhushan@srijan.in>
2016-11-04 16:21:16 +0530

    past webinar page added

--- srijan/webinars/past-webinar.php
+++ srijan/webinars/past-webinar.php
@@ -3,7 +3,7 @@
 <head>
   <meta charset="utf-8">
   <meta http-equiv="x-ua-compatible" content="ie=edge">
-  <title>Srijan | Webinars</title>
+  <title>Srijan | Past Webinars</title>
   <meta name="description" content="">
   <meta name="viewport" content="width=device-width, initial-scale=1">
 
@@ -14,126 +14,41 @@
 
   <script src="/js/vendor/modernizr-2.8.3.min.js"></script>
 </head>
-<body class="inner-page webinar-page">
+<body class="inner-page">
   <div class="canvas">
 
     <?php include '../nav.php';?>
 
-    <header class="section-page-header">
-      <figure class="parallax">
-        <img src="/img/webinar-bg.jpg" alt="graphics">
-      </figure>
-      <div class="page-wrap">
-        <span class="webinar-logo">
-          <?php echo file_get_contents("../img/webinar-logo.svg"); ?>
-        </span>
-      </div>
-    </header>
-
-
-    <main class="page section-landing webinars-listing">
-
-      <section class="upcoming-webinars content-block">
-        <h1 class="section-heading">Upcoming Webinar</h1>
-        <div class="webinar-item page-wrap">
-          <figure class="webinar-thumb">
-            <img src="/img/webinar-thumb.jpg" alt="webinar name">
-          </figure>
-          <article class="webinar-details">
-            <ul class="webinar-info">
-              <li><i class="fa fa-calendar"></i> 17 October 2016</li>
-              <li><i class="fa fa-clock-o"></i>  8:30 PM PST</li>
-            </ul>
-            <h2>Eliminate JavaScript CodeSmells</h2>
-            <div class="presenter-details">
-              <figure>
-                <img src="/img/morten.jpg" alt="morten" />
-              </figure>
-              <span class="presenter">Morten DK</span>
-            </div>
-            <p class="subtitle">Explore how to detect ‘code smells’, and identify several ways to deodorize the stench.</p>
-            <p>Have you ever written JavaScript that worked, but you didn't feel good about it? Maybe you knew it wasn't optimal, but you didn't..</p>
-
-            <a href="upcoming-webinar.php" class="btn webinar-btn">Learn More <i class="fa fa-angle-right"></i></a> 
-          </article>
-        </div>
-      </section>
-
-      <section class="content-block grey-bg">
-        <header class="section-subheader">
-          <h2>Featured Webinars</h2>
-        </header>
-        <div class="page-wrap">
-          <div class="row item-listing webinar-listing">
-            <div class="col-4 item ">
-              <div class="item-wrap">
-                <a href="#">
-                  <header>
-                    <figure class="icon">
-                      <img src="/img/larry-garfield.png" alt="larry garfield">
-                    </figure>
-
-                    <h2>Drupal 8 Crash Course <span class="by-txt">Larry Garfield</span></h2>
-                  </header>
-                  <div class="intro-txt">
-                    <p>Go from product idea to a prototype, swiftly, with Srijan’s Discovery Workshops.</p>
-                  </div>
-                </a>
-                <a href="webinar-details.php" class="cta-btn">Watch Webinar <i class="fa fa-angle-right"></i></a>
-              </div>
-            </div>
-            <div class="col-4 item ">
-              <div class="item-wrap">
-                <a href="#">
-                  <header>
-                    <span class="icon">
-                      <img src="/img/morten.jpg" alt="morten">
-                    </span>
-                    <h2>Goodbye to 10 Years of Theming Headaches <span class="by-txt">Morten DK</span></h2>
-                  </header>
-                  <div class="intro-txt">
-                    <p>Go from product idea to a prototype, swiftly, with Srijan’s Discovery Workshops.</p>
-                  </div>
-                </a>
-                <a href="webinar-details.php" class="cta-btn">Watch Webinar <i class="fa fa-angle-right"></i></a>
-
-              </div>
-            </div>
-            <div class="col-4 item ">
-              <div class="item-wrap">
-                <a href="#">
-                  <header>
-                    <span class="icon">
-                      <img src="/img/jesus.jpg" alt="">
-                    </span>
-
-                    <h2>Faster &amp; Smarter Development with Drupal Console <span class="by-txt">Jesus M. Olivas</span></h2>
-                  </header>
-                  <div class="intro-txt">
-                    <p>Go from product idea to a prototype, swiftly, with Srijan’s Discovery Workshops.</p>
-                  </div>
-                </a>
-                <a href="webinar-details.php" class="cta-btn">Watch Webinar <i class="fa fa-angle-right"></i></a>
-
-              </div>
-            </div>
-
-          </div>
-
-          <!-- <a href="listing.php" class="section-cta">View All Past Webinars <i class="fa fa-angle-right"></i></a> -->
-
-
-        </div>
-      </section>
-
-      <section class="content-block">
+    <main class="page section-landing past-webinar-page">
+      <section class="page-wrap guttered case-study">
         <header class="section-subheader">
           <h2>Past Webinars</h2>
         </header>
-        <div class="page-wrap past-webinar">
-          
+
+        <div class="cs-wrapper">
+          <div class="tag-filter">
+            <h3><i class="fa fa-filter"></i> Filter Webinars By:</h3>
+            <ul class="tag-list">
+              <li class="active"><a href="#">Recent</a></li>
+              <li><a href="#nogo">Agile</a></li>
+              <li><a href="#nogo">Drupal<i class="filter-icon"></i></a></li>
+              <li><a href="#nogo">UI/UX</a></li>
+              <li><a href="#nogo">Data Analysis</a></li>
+              <li><a href="#nogo">Company Culture</a></li>
+              <li><a href="#nogo">Enterpreneurship</a></li>
+              <li><a href="#nogo">Software Testing</a></li>
+              <li><a href="#nogo">Client Management</a></li>
+              <li><a href="#nogo">Digital Marketing</a></li>
+              <li><a href="#nogo">Community Building</a></li>
+              <li><a href="#nogo">Product Development</a></li>
+              <li><a href="#nogo">Mobile Apps</a></li>
+              <li><a href="#nogo">Website Development</a></li>
+              <li><a href="#nogo">Personal Development</a></li>
+              <li><a href="#nogo">Frontend Technologies</a></li>
+            </ul>
+          </div>
           <div class="row item-listing webinar-listing">
-            <div class="col-3 item ">
+            <div class="col-4 item ">
               <div class="item-wrap">
                 <a href="#">
                   <header>
@@ -150,7 +65,7 @@
                 <a href="webinar-details.php" class="cta-btn">Watch Webinar <i class="fa fa-angle-right"></i></a>
               </div>
             </div>
-            <div class="col-3 item ">
+            <div class="col-4 item ">
               <div class="item-wrap">
                 <a href="#">
                   <header>
@@ -167,7 +82,62 @@
 
               </div>
             </div>
-            <div class="col-3 item ">
+            <div class="col-4 item ">
+              <div class="item-wrap">
+                <a href="#">
+                  <header>
+                    <span class="icon">
+                      <img src="/img/jesus.jpg" alt="">
+                    </span>
+
+                    <h2>Faster &amp; Smarter Development with Drupal Console <span class="by-txt">Jesus M. Olivas</span></h2>
+                  </header>
+                  <div class="intro-txt">
+                    <p>Go from product idea to a prototype, swiftly, with Srijan’s Discovery Workshops.</p>
+                  </div>
+                </a>
+                <a href="webinar-details.php" class="cta-btn">Watch Webinar <i class="fa fa-angle-right"></i></a>
+
+              </div>
+            </div> 
+          </div>
+
+          <div class="row item-listing webinar-listing">
+            <div class="col-4 item ">
+              <div class="item-wrap">
+                <a href="#">
+                  <header>
+                    <figure class="icon">
+                      <img src="/img/larry-garfield.png" alt="larry garfield">
+                    </figure>
+
+                    <h2>Drupal 8 Crash Course Session on Frontend <span class="by-txt">Larry Garfield</span></h2>
+                  </header>
+                  <div class="intro-txt">
+                    <p>Go from product idea to a prototype, swiftly, with Srijan’s Discovery Workshops.</p>
+                  </div>
+                </a>
+                <a href="webinar-details.php" class="cta-btn">Watch Webinar <i class="fa fa-angle-right"></i></a>
+              </div>
+            </div>
+            <div class="col-4 item ">
+              <div class="item-wrap">
+                <a href="#">
+                  <header>
+                    <span class="icon">
+                      <img src="/img/morten.jpg" alt="morten">
+                    </span>
+                    <h2>Goodbye to 10 Years of Theming Headaches <span class="by-txt">Morten DK</span></h2>
+                  </header>
+                  <div class="intro-txt">
+                    <p>Go from product idea to a prototype, swiftly, with Srijan’s Discovery Workshops.</p>
+                  </div>
+                </a>
+                <a href="webinar-details.php" class="cta-btn">Watch Webinar <i class="fa fa-angle-right"></i></a>
+
+              </div>
+            </div>
+            <div class="col-4 item ">
               <div class="item-wrap">
                 <a href="#">
                   <header>
@@ -185,14 +155,52 @@
 
               </div>
             </div>
-            <div class="col-3 item ">
+          </div>
+
+          <div class="row item-listing webinar-listing">
+            <div class="col-4 item ">
+              <div class="item-wrap">
+                <a href="#">
+                  <header>
+                    <figure class="icon">
+                      <img src="/img/larry-garfield.png" alt="larry garfield">
+                    </figure>
+
+                    <h2>Drupal 8 Crash Course Session on Frontend <span class="by-txt">Larry Garfield</span></h2>
+                  </header>
+                  <div class="intro-txt">
+                    <p>Go from product idea to a prototype, swiftly, with Srijan’s Discovery Workshops.</p>
+                  </div>
+                </a>
+                <a href="webinar-details.php" class="cta-btn">Watch Webinar <i class="fa fa-angle-right"></i></a>
+              </div>
+            </div>
+            <div class="col-4 item ">
               <div class="item-wrap">
                 <a href="#">
                   <header>
                     <span class="icon">
                       <img src="/img/morten.jpg" alt="morten">
                     </span>
-                    <h2>Very Quick &amp; Dirty CSS removing in Drupal 8 <span class="by-txt">Morten DK</span></h2>
+                    <h2>Goodbye to 10 Years of Theming Headaches <span class="by-txt">Morten DK</span></h2>
+                  </header>
+                  <div class="intro-txt">
+                    <p>Go from product idea to a prototype, swiftly, with Srijan’s Discovery Workshops.</p>
+                  </div>
+                </a>
+                <a href="webinar-details.php" class="cta-btn">Watch Webinar <i class="fa fa-angle-right"></i></a>
+
+              </div>
+            </div>
+            <div class="col-4 item ">
+              <div class="item-wrap">
+                <a href="#">
+                  <header>
+                    <span class="icon">
+                      <img src="/img/jesus.jpg" alt="">
+                    </span>
+
+                    <h2>Faster &amp; Smarter Development with Drupal Console <span class="by-txt">Jesus M. Olivas</span></h2>
                   </header>
                   <div class="intro-txt">
                     <p>Go from product idea to a prototype, swiftly, with Srijan’s Discovery Workshops.</p>
@@ -204,11 +212,26 @@
             </div>
           </div>
 
-          <a href="past-webinar.php" class="section-cta">View All Past Webinars <i class="fa fa-angle-right"></i></a>
-        </div>
-      </section>
+          <ul class="pagination">
+              <li class="active"><a href="#">1</a></li>
+              <li><a href="#">2</a></li>
+              <li><a href="#">3</a></li>
+              <li><a href="#">4</a></li>
+              <li><a href="#">5</a></li>
+              <li><a href="#">6</a></li>
+              <li><a href="#">7</a></li>
+              <li><a href="#">8</a></li>
+              <li><a href="#">9</a></li>
+              <li><a href="#">10</a></li>
+              <li><a href="#">..</a></li>
+              <li><a href="#">15</a></li>
+              <li><a href="#">..</a></li>
+              <li><a href="#">Next <i class="fa fa-angle-right"></i></a></li>
+              <li><a href="#">Last <i class="fa fa-angle-right"></i><i class="fa fa-angle-right"></i></a></li>
+            </ul>
+        </section>
 
-      <section class="testimonial-cta social-webinar">
+        <section class="testimonial-cta social-webinar">
         <img src="/img/webinar-social.jpg" alt="" class="testimonial-bg">
         <div class="page-wrap">
           <div class="row">
@@ -234,29 +257,21 @@
         </div>
       </section>
 
-      <section class="become-speaker content-block bottom-float">
-        <div class="page-wrap row">
-          <p class="col-8">If you are interested in leading one of our webinars as a speaker or a panelist, drop us a mail at <a href="mailto:email@mail.com">webinars@srijan.in</a>.
+</main>
 
-          </p>
-          <a href="#" class="btn cta-btn">Become a Speaker</a>
-        </div>
-      </section>
-
-      <?php include '../contact-form.php';?>
-    </main>
-
-    <?php include '../footer.php';?>
-
-  </div>
-  <?php include '../mobile-nav.php';?>
+<?php include '../contact-form.php';?>
+<?php include '../footer.php';?>
 
 
+</div>
 
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
-  <script>window.jQuery || document.write('<script src="/js/vendor/jquery-1.11.3.min.js"><\/script>')</script>
-  <script src="/js/plugins.js"></script>
-  <script src="/js/main.js"></script>
+<?php include '../mobile-nav.php';?>
+
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+<script>window.jQuery || document.write('<script src="/js/vendor/jquery-1.11.3.min.js"><\/script>')</script>
+<script src="/js/plugins.js"></script>
+<script src="/js/main.js"></script>
 
 </body>
 </html>
